24fda9895be8f8f99c5b7725e5d842cc0196db58
Kristján Oddsson <koddsson@gmail.com>
2022-10-27 20:33:33 +0200

    Update some stuff

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,20 @@
-// Put the code here
+function ready(): Promise<void> {
+  return new Promise(resolve => {
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+      resolve()
+    } else {
+      document.addEventListener('DOMContentLoaded', () => resolve())
+    }
+  })
+}
+
+await ready()
+const fullScreenButton = document.querySelector('button')
+const input = document.querySelector('textarea')
+fullScreenButton?.addEventListener('click', async () => {
+  await input?.requestFullscreen()
+  input?.focus()
+})
+
+export {}
+
