a9ff7570bd02d124e7cc11f272476d752c4a0de3
Keith Cirkel <keithamus@users.noreply.github.com>
2023-11-11 10:40:41 +0000

    use invokers

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,19 +1,10 @@
-function ready(): Promise<void> {
-  return new Promise(resolve => {
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-      resolve()
-    } else {
-      document.addEventListener('DOMContentLoaded', () => resolve())
-    }
-  })
-}
-
-await ready()
 const fullScreenButton = document.querySelector('button')
 const input = document.querySelector('textarea')
-fullScreenButton?.addEventListener('click', async () => {
-  await input?.requestFullscreen()
-  input?.focus()
-})
+if (!fullScreenButton.invokeTargetElement) {
+  fullScreenButton?.addEventListener('click', async () => {
+    await input?.requestFullscreen()
+    input?.focus()
+  })
+}
 
 export {}
